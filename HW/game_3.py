4e153ce83c78dbb3a031584c34ffd143f115ff5a
aapashk <aapashka@gmail.com>
2023-11-16 14:42:19 +0300

    Добавлена папка для домашнего задания

---
+++ HW/game_3.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    pred_min=1
+    pred_max=100
+    while True:
+        count += 1
+        predict_number = np.random.randint(pred_min, pred_max+1)  # предполагаемое число
+        if predict_number>number:
+            pred_max=predict_number
+        if predict_number<number:
+            pred_min=predict_number
+        if number == predict_number:
+            break  # выход из цикла если угадали
+    return count
+
+#print(f'Количество попыток: {random_predict(10)}' )
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
